09bc45946159c27abaeb62273b1ef5d1b2d9adfa
Aly <71610406+alisaadghanem@users.noreply.github.com>
2024-07-06 20:32:09 +0300

    fix cards display

--- src/components/ShotItem.vue
+++ src/components/ShotItem.vue
@@ -294,8 +294,6 @@ const dropdownStyle = {
   opacity: 0;
 }
 
-/* Media Queries for responsiveness */
-
 @media (max-width: 1200px) {
   .projects-grid {
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,5 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
     <ProfileHeader />
     <ShotGallery />
     <ShotItem />
@@ -9,8 +8,8 @@
 </template>
 
 <script setup>
-import ProfileHeader from 'src/components/ProfileHeader.vue';
-import ShotGallery from 'src/components/ShotGallery.vue';
-import ShotItem from 'src/components/ShotItem.vue';
-import Footer from 'src/components/MainFooter.vue';
+import ProfileHeader from "src/components/ProfileHeader.vue";
+import ShotGallery from "src/components/ShotGallery.vue";
+import ShotItem from "src/components/ShotItem.vue";
+import Footer from "src/components/MainFooter.vue";
 </script>
